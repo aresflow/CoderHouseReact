f267646c66926b6bc48ff0371bac7f141fe57f19
mellamoeze <ezequielmaldonado97@hotmail.com>
2022-06-01 21:27:48 -0300

    Clase 3 - Add Cart and List Container - Maldonado

--- tienda-maldonado/src/App.js
+++ tienda-maldonado/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/NavBar';
+import './css/App.css';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
+  const greeting = "Hola soy Item List Container"
+
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <ItemListContainer greeting={greeting} />
+    </>
   );
 }
 
