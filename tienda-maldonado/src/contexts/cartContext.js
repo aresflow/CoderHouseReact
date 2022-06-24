7f16cb9e6abc400d3956476ea2d288d428442143
mellamoeze <ezequielmaldonado97@hotmail.com>
2022-06-23 22:16:04 -0300

    Clase 9 - fix commit

--- tienda-maldonado/src/contexts/cartContext.js
+++ tienda-maldonado/src/contexts/cartContext.js
@@ -7,24 +7,12 @@ export const CartContextProvider = ({ children }) => {
     const [cart, setCart] = useState([])
 
     const addToCart = (item) => {
-        if(cart.length > 0){
-            cart.find(p => {
-                if(p.id === item.id) {
-                    p.cantidad += item.cantidad
-                    
-                }
-                else {
-                    setCart([...cart, item])
-                }
-            })
-        }
-        // }
-            // console.log(cartItem, "console del cartitem")
-            // setCart([...cartItem, item])
-        // }
-        else {
-            setCart([...cart, item])
-        }
+        if (cart.find(p => p.id === item.id)) {
+            cart.map((p) => { if (p.id === item.id) { 
+                p.cantidad += item.cantidad; 
+                setCart([...cart]) }});}
+        else 
+            setCart([...cart, item]);
     }
 
     const removeCart = () => {
